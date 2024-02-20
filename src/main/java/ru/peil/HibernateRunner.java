606204a9bcfa7fcd004388fb0f14910002f11e26
annapeyl <annapeyl@gmail.com>
2024-02-20 16:46:41 +0600

    [+] added entity

--- src/main/java/ru/peil/HibernateRunner.java
+++ src/main/java/ru/peil/HibernateRunner.java
@@ -2,15 +2,32 @@ package ru.peil;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
 import org.hibernate.cfg.Configuration;
+import ru.peil.models.User;
+
+import java.time.LocalDate;
 
 public class HibernateRunner {
     public static void main(String[] args) {
         Configuration configuration = new Configuration();
+        //configuration.addAnnotatedClass(User.class);
+        //кэмел кейс преобразовывает в формат называний полей в бд
+        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
         configuration.configure();
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
+            session.beginTransaction();
             System.out.println("OK");
+            User user = User.builder()
+                    .username("anna@gmail.com")
+                    .lastname("Frolova1")
+                    .firstname("Anna")
+                    .birthDate(LocalDate.of(2000, 4, 23))
+                    .age(23)
+                    .build();
+            session.save(user);
+            session.getTransaction().commit();
         }
     }
 }
---
+++ src/main/java/ru/peil/models/User.java
@@ -0,0 +1,23 @@
+package ru.peil.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Table(name = "users")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    private String username;
+    private String firstname;
+    private String lastname;
+    private LocalDate birthDate;
+    private Integer age;
+}
